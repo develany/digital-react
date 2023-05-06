04b64d118bf1fca72508fd231b8ce5c760631ef9
Elany Peixoto <elanysantos5@outlook.com>
2023-05-05 21:04:31 -0300

    componente tabela

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import Apresentacao from "./Apresentacao";
 import Button from "./Button";
 import "./Global.css"
+import Tabela from "./Tabela";
 function App(){
+  const data = [{id:1, nome: 'elany', usuario:'elany1', email: 'elany@gmail.com'},{id:2, nome: 'john', usuario:'john1', email: 'john@gmail.com'},{id:3, nome: 'aladdim', usuario:'aladdim1', email: 'aladdin@gmail.com'}, ]
   return ( 
+    <>
       <div id="app">
         <Apresentacao></Apresentacao>
         <Button tipo='primary'>Primary</Button>
         <Button tipo='danger'>Primary</Button>
         <Button disabled >Primary</Button>
+        
        </div>
+       <div id= 'tabela'>
+       <Tabela data={data}></Tabela>
+       </div>
+    </>
   )
 }
 export default App;
---
+++ src/Tabela.jsx
@@ -0,0 +1,26 @@
+function Tabela (props) {
+    return(
+        <table>
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>USÁRIO</th>
+                <th>E-MAIL</th>
+                </tr>
+            </thead>   
+            <tbody>
+                {props.data.map(props => (
+          <tr key={props.id}>
+            <td>{props.id}</td>
+            <td>{props.nome}</td>
+            <td>{props.usuario}</td>
+            <td>{props.email}</td>
+          </tr>
+        ))}
+
+            </tbody>    
+        </table>
+    )
+}
+export default Tabela;
